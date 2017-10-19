a5484e2df26114df4dc651a2a602f86cdbde4178
Encho Belezirev <encho.belezirev@sap.com>
2017-10-19 22:21:11 +0300

    Add method for executing actions

--- clients/mtaclient/mta_rest_client.go
+++ clients/mtaclient/mta_rest_client.go
@@ -1,11 +1,13 @@
 package mtaclient
 
 import (
+	"context"
 	"net/http"
 	"os"
 
 	baseclient "github.com/SAP/cf-mta-plugin/clients/baseclient"
 	models "github.com/SAP/cf-mta-plugin/clients/models"
+	operations "github.com/SAP/cf-mta-plugin/clients/mtaclient/operations"
 	strfmt "github.com/go-openapi/strfmt"
 )
 
@@ -23,26 +25,40 @@ func NewMtaClient(host, spaceID string, rt http.RoundTripper, jar http.CookieJar
 	return &MtaRestClient{baseclient.BaseClient{TokenFactory: tokenFactory}, client: httpMtaClient}
 }
 
-func ExecuteAction(operationID, actionID string) (ResponseHeader, error) {
+func (c MtaRestClient) ExecuteAction(operationID, actionID string) (ResponseHeader, error) {
+	params := &operations.ExecuteOperationActionParams{
+		OperationID: operationID,
+		ActionID: actionID,
+		Context: context.TODO()
+	}
+	token, err := c.TokenFactory.NewToken()
+	if err != nil {
+		return ResponseHeader{}, baseclient.NewClientError(err)
+	}
+	// TODO: the token must be added to the accepting parameters. This should be done through the swagger. See the `auth` section :)
+	resp, err := c.client.Operations.ExecuteOperationAction(params)
+	if err != nil {
+		return ResponseHeader{}, baseclient.NewClientError(err)
+	}
+	return ResponseHeader{Location: resp.Payload}, nil
+}
+func (client MtaRestClient) GetMta(mtaID string) (*models.Mta, error) {
 
 }
-func GetMta(mtaID string) (*models.Mta, error) {
+func (client MtaRestClient) GetMtaFiles() ([]*File, error) {
+}
+func (client MtaRestClient) GetMtaOperation(operationID, embed string) (*models.Operation, error) {
 
 }
-func GetMtaFiles() ([]*File, error) {
+func (client MtaRestClient) GetMtaOperationLogs(operationID string) ([]*models.Log, error) {
 }
-func GetMtaOperation(operationID, embed string) (*models.Operation, error) {
-
+func (client MtaRestClient) GetMtaOperations() ([]*models.Operation, error) {
 }
-func GetMtaOperationLogs(operationID string) ([]*models.Log, error) {
+func (client MtaRestClient) GetMtas() ([]*models.Mta, error) {
 }
-func GetMtaOperations() ([]*models.Operation, error) {
+func (client MtaRestClient) GetOperationActions(operationID string) ([]string, error) {
 }
-func GetMtas() ([]*models.Mta, error) {
+func (client MtaRestClient) StartMtaOperation(operation models.Operation) (ResponseHeader, error) {
 }
-func GetOperationActions(operationID string) ([]string, error) {
-}
-func StartMtaOperation(operation models.Operation) (ResponseHeader, error) {
-}
-func UploadMtaFile(file os.File) (*models.File, error) {
+func (client MtaRestClient) UploadMtaFile(file os.File) (*models.File, error) {
 }
