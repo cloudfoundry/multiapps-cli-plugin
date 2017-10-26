b738f6ef87163dea189d8a5bca01fce125a92381
Encho Belezirev <encho.belezirev@sap.com>
2017-10-26 11:31:34 +0300

    Remove non-used methods from restclient

--- clients/restclient/rest_client.go
+++ clients/restclient/rest_client.go
@@ -7,7 +7,6 @@ import (
 	"github.com/go-openapi/strfmt"
 
 	"github.com/SAP/cf-mta-plugin/clients/baseclient"
-	"github.com/SAP/cf-mta-plugin/clients/models"
 	operations "github.com/SAP/cf-mta-plugin/clients/restclient/operations"
 )
 
@@ -22,57 +21,6 @@ func NewRestClient(host, org, space string, rt http.RoundTripper, jar http.Cooki
 	t := baseclient.NewHTTPTransport(host, getRestURL(org, space), getRestURL(baseclient.EncodeArg(org), baseclient.EncodeArg(space)), rt, jar)
 	client := New(t, strfmt.Default)
 	return RestClient{baseclient.BaseClient{tokenFactory}, client}
-}
-
-// GetOperations retrieves all ongoing operations
-func (c RestClient) GetOperations(lastRequestedOperations *string, requestedStates []string) (models.Operations, error) {
-	params := &operations.GetOperationsParams{
-		Last:    lastRequestedOperations,
-		Status:  requestedStates,
-		Context: context.TODO(),
-	}
-	token, err := c.TokenFactory.NewToken()
-	if err != nil {
-		return models.Operations{}, baseclient.NewClientError(err)
-	}
-	resp, err := c.Client.Operations.GetOperations(params, token)
-	if err != nil {
-		return models.Operations{}, baseclient.NewClientError(err)
-	}
-	return resp.Payload, nil
-}
-
-// GetComponents retrieves all deployed components (MTAs and standalone apps)
-func (c RestClient) GetComponents() (*models.Components, error) {
-	params := &operations.GetComponentsParams{
-		Context: context.TODO(),
-	}
-	token, err := c.TokenFactory.NewToken()
-	if err != nil {
-		return nil, baseclient.NewClientError(err)
-	}
-	resp, err := c.Client.Operations.GetComponents(params, token)
-	if err != nil {
-		return nil, baseclient.NewClientError(err)
-	}
-	return resp.Payload, nil
-}
-
-// GetMta retrieves the deployed MTA with the specified MTA ID
-func (c RestClient) GetMta(mtaID string) (*models.Mta, error) {
-	params := &operations.GetMtaParams{
-		MtaID:   mtaID,
-		Context: context.TODO(),
-	}
-	token, err := c.TokenFactory.NewToken()
-	if err != nil {
-		return nil, baseclient.NewClientError(err)
-	}
-	resp, err := c.Client.Operations.GetMta(params, token)
-	if err != nil {
-		return nil, baseclient.NewClientError(err)
-	}
-	return resp.Payload, nil
 }
 
 func getRestURL(org, space string) string {
