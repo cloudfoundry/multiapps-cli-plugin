f81894393f7f2795f9c69618cd8148de0eb43798
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-06-12 13:41:41 +0300

    Fix monitor action
    JIRA: LMCROSSITXSADEPLOY-1070

--- commands/monitor_action.go
+++ commands/monitor_action.go
@@ -15,23 +15,15 @@ type MonitorAction struct {
 
 // Execute executes monitor action on process with the specified id
 func (a *MonitorAction) Execute(operationID string, mtaClient mtaclient.MtaClientOperations, sessionProvider csrf.SessionProvider) ExecutionStatus {
-
 	err := sessionProvider.GetSession()
 	if err != nil {
-		ui.Failed("Could not retrieve x-csrf-token for the current session: %s", baseclient.NewClientError(err))
+		ui.Failed("Could not retrieve CSRF token for the current session: %s", baseclient.NewClientError(err))
 		return Failure
 	}
-
-	_, err = mtaClient.ExecuteAction(operationID, "monitor")
-	if err != nil {
-		ui.Failed("Could not monitor multi-target app operation with id %s: %s", terminal.EntityNameColor(operationID), err)
-		return Failure
-	}
-	ui.Ok()
 
 	operation, err := getMonitoringOperation(operationID, mtaClient)
 	if err != nil {
-		ui.Failed("Could not monitor multi-target app operation with id %s: %s", terminal.EntityNameColor(operationID), err)
+		ui.Failed("Could not monitor operation %s: %s", terminal.EntityNameColor(operationID), err)
 		return Failure
 	}
 
