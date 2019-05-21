089f1d3aa02c6deeb9bfd64258ef45c08620716c
Vasil Bozhurski <vasil.bozhurski@sap.com>
2019-05-21 16:21:40 +0300

    enhance build script to output in specific folder
    this changes the build.sh script to output in the build folder and also
    include some extra metadata files such as version and changelog for the
    specific version.

--- build.sh
+++ build.sh
@@ -18,8 +18,26 @@ function copyPluginsWithOldNames() {
     cp $PLUGIN_NAME_OSX $OLD_PLUGIN_NAME_OSX
 }
 
+function movePluginsToBuildFolder() {
+    local folder=$1
+    mv $PLUGIN_NAME_WIN_32 $folder
+    mv $PLUGIN_NAME_WIN_64 $folder
+    mv $PLUGIN_NAME_LINUX_32 $folder
+    mv $PLUGIN_NAME_LINUX_64 $folder
+    mv $PLUGIN_NAME_OSX $folder
+}
+
+function createBuildMetadataFiles() {
+    local version=$1
+    local folder=$2
+	echo -n "v${version}" > ${folder}/version
+	grep -Pzoa "(?s)## v${version}(.*?)##" CHANGELOG.md | grep -va "##" | tr -s '\n' '\n' > ${folder}/changelog
+}
+
 script_dir="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
 cd "${script_dir}"
+
+BUILD_FOLDER=build
 PLUGIN_NAME_WIN_32=multiapps-plugin.win32
 PLUGIN_NAME_WIN_64=multiapps-plugin.win64
 PLUGIN_NAME_LINUX_32=multiapps-plugin.linux32
@@ -29,15 +47,15 @@ OLD_PLUGIN_NAME_WIN_64=mta_plugin_windows_amd64.exe
 OLD_PLUGIN_NAME_LINUX_64=mta_plugin_linux_amd64
 OLD_PLUGIN_NAME_OSX=mta_plugin_darwin_amd64
 
-if [[ $# -ne 1 ]]; then
-        echo "usage: ${0} <version>"
-        exit 1
-fi
-version=$1
+version=$(<cfg/VERSION)
 build $version linux 386 $PLUGIN_NAME_LINUX_32
 build $version linux amd64 $PLUGIN_NAME_LINUX_64
 build $version windows 386 $PLUGIN_NAME_WIN_32
 build $version windows amd64 $PLUGIN_NAME_WIN_64
 build $version darwin amd64 $PLUGIN_NAME_OSX
 
+mkdir $BUILD_FOLDER -p
+createBuildMetadataFiles $version $BUILD_FOLDER
+movePluginsToBuildFolder $BUILD_FOLDER
+cd $BUILD_FOLDER
 copyPluginsWithOldNames
