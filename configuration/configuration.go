69cf5a94817161ec6b155c6d7fe97ba678c20f10
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-01-28 13:28:58 +0200

    Use consistent naming for configurations
    Environment variables used for configuration of our plugin now follow
    the conventions of CF's CLI.

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -11,8 +11,10 @@ const (
 )
 
 var ChunkSizeInMBConfigurableProperty = configurableProperty{
-	Name:                  "CHUNK_SIZE_IN_MB",
-	DeprecatedNames:       []string{},
+	Name: "MULTIAPPS_UPLOAD_CHUNK_SIZE",
+	DeprecatedNames: []string{
+		"CHUNK_SIZE_IN_MB",
+	},
 	Parser:                chunkSizeInMBParser{},
 	ParsingSuccessMessage: "Attention: You've specified a custom chunk size (%d MB) via the environment variable \"%s\".\n",
 	ParsingFailureMessage: "Attention: You've specified an INVALID custom chunk size (%s) via the environment variable \"%s\". Using default: %d\n",
@@ -20,8 +22,10 @@ var ChunkSizeInMBConfigurableProperty = configurableProperty{
 }
 
 var BackendURLConfigurableProperty = configurableProperty{
-	Name:                  "DEPLOY_SERVICE_URL",
-	DeprecatedNames:       []string{},
+	Name: "MULTIAPPS_CONTROLLER_URL",
+	DeprecatedNames: []string{
+		"DEPLOY_SERVICE_URL",
+	},
 	Parser:                noOpParser{},
 	ParsingSuccessMessage: "Attention: You've specified a custom backend URL (%s) via the environment variable \"%s\". The application listening on that URL may be outdated, contain bugs or unreleased features or may even be modified by a potentially untrused person. Use at your own risk.\n",
 	ParsingFailureMessage: "No validation implemented for custom backend URLs. If you're seeing this message then something has gone horribly wrong.\n",
